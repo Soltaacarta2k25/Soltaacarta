2e624dc5779acccb0f0efaae2776d3741db1ac76
Gustavo <gscalambrini@icloud.com>
2025-05-14 20:54:13 -0300

    fix(repository): remove o submódulo da pasta generate-ai e atualiza arquivo .env

---
+++ README.md
@@ -0,0 +1,6 @@
+# Soltaacarta
+Gabriel Gardilin Passos Avelino  823124421 
+Giovanni Bolsoni Fernandes 824125317
+Kayque Braga da Silva 824118930  
+Pedro Cardoso de Lima Patruceli  823211483
+Gustavo Scalambrini  - 823213506
---
+++ generate-ai/src/Header.jsx
@@ -0,0 +1,22 @@
+function Header() {
+    return (
+      <header style={{ textAlign: 'center', marginBottom: '2rem'}}>
+        <h1>
+          <i
+            className="fa-solid fa-robot"
+            style={{ marginRight: '10px', color: '#1d4ed8' }}
+          ></i>
+          GENERATE AI 
+        </h1>
+        <p><strong>Use a inteligência artificial para criar textos incríveis!</strong></p>
+
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+        />
+      </header>
+    )
+  }
+  
+  export default Header
+  
